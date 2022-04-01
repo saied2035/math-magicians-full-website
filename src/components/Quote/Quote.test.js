6b12047af262bf58aa59b45195769a1e6b004b8f
bushmusi <bushera.mestofa@gmail.com>
2022-04-01 12:07:05 +0300

    Add quote test

---
+++ src/components/Quote/Quote.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import renderer from 'react-test-renderer';
+
+import Quote from "./Quote";
+
+it ('Rederer for Home page test', () => {
+    const tree = renderer.create(<Quote />).toJSON();
+    expect(tree).toMatchSnapshot();
+})
