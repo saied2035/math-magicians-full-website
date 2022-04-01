b392ac8e11dadb964e6f189e56fd8c54a7ee4784
bushmusi <bushera.mestofa@gmail.com>
2022-04-01 12:16:04 +0300

    Add navbar test

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -1,9 +1,14 @@
 import React from "react";
 import renderer from 'react-test-renderer';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import App from '../../App'
+import Navbar from './Navbar';
 
 it('Check all links are listed', () => {
-    const tree = renderer.create(<App />).toJSON();
+    const tree = renderer.create(
+        <Router>
+            <Navbar />
+        </Router>
+    ).toJSON();
     expect(tree).toMatchSnapshot();
 })
