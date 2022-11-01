f3d0065783cbadf48a10d5fc6e945c24335b7efe
Saied Gaffer <saied2421998@gmail.com>
2022-11-01 09:17:13 +0200

    few changes

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,7 +1,7 @@
 import './Quote.css';
 
 const Quote = () => (
-  <p className="flex flex-column justify-center tc f3" style={{ height: '80vh', gap: '1rem' }}>
+  <p className="flex flex-column justify-center tc f3" style={{ height: '70vh', gap: '1rem' }}>
     Without mathematics, there’s nothing you can do. Everything around you is mathematics.
     Everything around you is numbers.
     <span className="QuoteAuthor"> — Shakuntala Devi, Indian writer and mental calculator</span>
